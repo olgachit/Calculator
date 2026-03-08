679a4c83878c99458634c967ac928cef478cbed2
Olga Chitembo <olga.chitembo@metropolia.fi>
2026-03-08 17:29:50 +0200

    clone https://github.com/ADirin/week7_calculator_fx_db.git

---
+++ src/main/java/CalculatorController.java
@@ -0,0 +1,30 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CalculatorController {
+
+    @FXML private TextField number1Field;
+    @FXML private TextField number2Field;
+    @FXML private Label resultLabel;
+
+    @FXML
+    private void onCalculateClick() {
+        try {
+            double num1 = Double.parseDouble(number1Field.getText());
+            double num2 = Double.parseDouble(number2Field.getText());
+
+            double sum = num1 + num2;
+            double product = num1 * num2;
+
+            resultLabel.setText("Sum: " + sum + ", Product: " + product);
+
+            // Save to DB
+            ResultService.saveResult(num1, num2, sum, product);
+
+        } catch (NumberFormatException e) {
+            resultLabel.setText("Please enter valid numbers!");
+        }
+    }
+}
